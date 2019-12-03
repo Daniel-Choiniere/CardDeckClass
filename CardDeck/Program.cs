dee4018474b3f0870131f157655e58a125b537fa
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-12-02 19:18:03 -0500

    basic class to create and shuffle a card deck. Can be reused and modified for future projects

---
+++ CardDeck/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CardDeck
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var deck1 = Deck.CreateFullDeck();
+            deck1.Shuffle();
+            
+            foreach (var item in deck1.Cards)
+            {
+                Console.WriteLine("{0} {1}",  item.Number, item.Suit);
+            }
+        }
+    }
+}
---
+++ CardDeck/Suit.cs
@@ -0,0 +1,10 @@
+namespace CardDeck
+{
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+}
